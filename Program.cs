2938076993b7c04362a93b6b9a2ea260f3e31fef
Hadeed Ahmad <hadeedji@gmail.com>
2024-10-05 13:39:10 +0500

    Log error while startup

--- Program.cs
+++ Program.cs
@@ -2,30 +2,43 @@ using System.Data;
 using System.Data.SQLite;
 using Dapper;
 using Serilog;
+using Serilog.Events;
 using sproj;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
-    .WriteTo.Console()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
     .WriteTo.Seq("http://localhost:5341")
+    .WriteTo.Console()
     .CreateLogger();
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+try {
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog();
-builder.Services.AddSingleton<IDbConnection>(new SQLiteConnection("Data Source=database.db"));
+    // Register services
+    builder.Host.UseSerilog();
+    builder.Services.AddSingleton<IDbConnection>(new SQLiteConnection("Data Source=database.db"));
 
-WebApplication app = builder.Build();
+    WebApplication app = builder.Build();
 
-InitDb(app.Services.GetRequiredService<IDbConnection>());
+    InitDb(app.Services.GetRequiredService<IDbConnection>());
 
-app.MapGet("/users", async (IDbConnection connection) => await connection.QueryAsync("select id, username from users;"));
-app.MapPost("/users", async (User user, IDbConnection connection) => {
-    await connection.ExecuteAsync("insert into users (username, password) values (@username, @password);", user);
-    return Results.Created();
-});
+    // Middleware
+    app.UseSerilogRequestLogging();
 
-app.Run();
+    app.MapGet("/users",
+        async (IDbConnection connection) => await connection.QueryAsync("select id, username from users;"));
+    app.MapPost("/users", async (User user, IDbConnection connection) => {
+        await connection.ExecuteAsync("insert into users (username, password) values (@username, @password);", user);
+        return Results.Created();
+    });
+
+    app.Run();
+} catch (Exception ex) {
+    Log.Fatal(ex, "Application terminated unexpectedly");
+} finally {
+    Log.CloseAndFlush();
+}
 
 void InitDb(IDbConnection connection) {
     var sqlFilePath = "init_db.sql";
