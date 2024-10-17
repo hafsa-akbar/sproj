b285b35706c68880b44c0a25927b5c211888158c
Hadeed Ahmad <hadeedji@gmail.com>
2024-10-18 04:22:41 +0500

    Move user routes to separate file

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using System.Data;
 using System.Data.SQLite;
 using Dapper;
 using Serilog;
-using sproj;
+using sproj.Routes;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Verbose()
@@ -14,25 +14,26 @@ try {
 
     builder.Services.AddSerilog(lc => lc.ReadFrom.Configuration(builder.Configuration));
     builder.Services.AddProblemDetails();
-    
+
     if (builder.Environment.IsDevelopment()) {
+        var connectionString = builder.Configuration.GetConnectionString("SQLite");
+        builder.Services.AddSingleton<IDbConnection>(new SQLiteConnection(connectionString));
+    } else {
+        // TODO: Move to production database
         var connectionString = builder.Configuration.GetConnectionString("SQLite");
         builder.Services.AddSingleton<IDbConnection>(new SQLiteConnection(connectionString));
     }
 
     WebApplication app = builder.Build();
 
+    if (!app.Environment.IsDevelopment()) app.UseExceptionHandler();
+    app.UseStatusCodePages();
+
     InitDb(app.Services.GetRequiredService<IDbConnection>());
 
     app.UseSerilogRequestLogging();
 
-    app.MapGet("/users",
-        async (IDbConnection connection) => await connection.QueryAsync("select id, username from users;"));
-    app.MapPost("/users", async (User user, IDbConnection connection) => {
-        await connection.ExecuteAsync("insert into users (username, password) values (@username, @password);", user);
-        return Results.Created();
-    });
-    app.MapGet("/error", void () => throw new Exception());
+    app.MapGroup("/users").UseUserRoutes();
 
     app.Run();
 } catch (Exception ex) {
@@ -45,6 +46,6 @@ void InitDb(IDbConnection connection) {
     var sqlFilePath = "init_db.sql";
     var createTableQuery = File.ReadAllText(sqlFilePath);
     connection.Execute(createTableQuery);
-    
+
     Log.Information("Database successfully initialized");
 }
