c531382905b2ed43e735345db949929d91e60c3d
Hadeed Ahmad <hadeedji@gmail.com>
2024-10-24 12:06:54 +0500

    Use minimalapi

---
+++ Endpoints/UserEndpoints.cs
@@ -0,0 +1,67 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace sproj.Endpoints;
+
+public static class UserEndpoints {
+    public static void RegisterUserEndpoints(this IEndpointRouteBuilder app) {
+        RouteGroupBuilder group = app.MapGroup("user");
+
+        group.MapPost("/register", RegisterEndpoint);
+        group.MapPost("/login", LoginEndpoint);
+    }
+
+    public static async Task<IResult> RegisterEndpoint(RegisterDTO input,
+        UserManager<IdentityUser> userManager) {
+        var user = new IdentityUser { UserName = input.UserName };
+        IdentityResult result = await userManager.CreateAsync(user, input.Password);
+
+        if (result.Succeeded)
+            return Results.Ok();
+
+        return Results.BadRequest(result.Errors);
+    }
+
+    public static async Task<IResult> LoginEndpoint(LoginDTO input, UserManager<IdentityUser> userManager,
+        IConfiguration config) {
+        IdentityUser? user = await userManager.FindByNameAsync(input.UserName);
+        if (user == null) return Results.Unauthorized();
+
+        var passwordCheck = await userManager.CheckPasswordAsync(user, input.Password);
+        if (!passwordCheck) return Results.Unauthorized();
+
+        var claims = new List<Claim> {
+            new(JwtRegisteredClaimNames.Sub, user.UserName!),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        claims.AddRange(userRoles.Select(role => new Claim("role", role)));
+
+        var userClaims = await userManager.GetClaimsAsync(user);
+        claims.AddRange(userClaims);
+
+        var jwtKey = config["JWT:Key"] ?? "00000000000000000000000000000000";
+        var tokenDuration = int.Parse(config["JWT:Duration"] ?? "900");
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddSeconds(tokenDuration),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+                SecurityAlgorithms.HmacSha256)
+        );
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return Results.Ok(new
+            { token = tokenString, expires_in = tokenDuration, token_type = JwtBearerDefaults.AuthenticationScheme });
+    }
+
+    public record struct RegisterDTO(string UserName, string Password);
+
+    public record struct LoginDTO(string UserName, string Password);
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using sproj.Endpoints;
 using sproj.Models;
 
 Log.Logger = new LoggerConfiguration()
@@ -15,90 +15,61 @@ Log.Logger = new LoggerConfiguration()
 try {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddServices(builder.Configuration);
+    builder.RegisterServices();
 
     WebApplication app = builder.Build();
-
-    app.UseRouting();
-
-    if (app.Environment.IsDevelopment()) {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseDeveloperExceptionPage();
-    }
-    else app.UseExceptionHandler();
-
-    app.UseStatusCodePages();
-    app.UseSerilogRequestLogging();
-
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-    app.MapControllers();
-    app.MapGet("/", (ClaimsPrincipal user) => "You are logged in!").RequireAuthorization();
+    
+    app.RegisterMiddleware();
 
     app.Run();
-} catch (HostAbortedException _) { } catch (Exception ex) {
+} catch (HostAbortedException) { } catch (Exception ex) {
     Log.Fatal(ex, "Application terminated unexpectedly");
 } finally {
     Log.CloseAndFlush();
 }
 
-public static class ServiceCollectionExtensions {
-    private static IConfiguration _config;
-
-    public static void AddServices(this IServiceCollection services, IConfiguration config) {
-        _config = config;
-
-        services
-            .AddSerilog()
-            .AddSwagger()
-            .AddProblemDetails()
-            .AddControllers();
-
-        services
-            .AddDataServices()
-            .AddAuthServices();
-    }
-
-    private static IServiceCollection AddSerilog(this IServiceCollection services) {
-        services.AddSerilog(logging => logging.ReadFrom.Configuration(_config));
-        return services;
-    }
-
-    private static IServiceCollection AddSwagger(this IServiceCollection services) {
-        services.AddEndpointsApiExplorer()
-            .AddSwaggerGen();
-        return services;
-    }
-
-    private static IServiceCollection AddDataServices(this IServiceCollection services) {
-        var connectionString = _config.GetConnectionString("DefaultConnection");
-
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(connectionString)
-                .UseLowerCaseNamingConvention());
-
-        services.AddIdentityCore<IdentityUser>()
-            .AddEntityFrameworkStores<AppDbContext>();
-
-        return services;
-    }
-
-    private static IServiceCollection AddAuthServices(this IServiceCollection services) {
-        var jwtKey = _config["JWT:Key"];
-
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+public static class StartupExtensions {
+    public static void RegisterServices(this WebApplicationBuilder builder) {
+        builder.Services.AddSerilog(logging => logging.ReadFrom.Configuration(builder.Configuration));
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddProblemDetails();
+        
+        
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddIdentityCore<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(connectionString).UseLowerCaseNamingConvention());
+        
+        
+        var jwtKey = builder.Configuration["JWT:Key"] ?? "00000000000000000000000000000000";
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options => {
-                options.TokenValidationParameters = new TokenValidationParameters {
-                    ValidateAudience = false,
-                    ValidateIssuer = false,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-                };
                 options.MapInboundClaims = false;
+                options.TokenValidationParameters = new TokenValidationParameters {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
             });
+        builder.Services.AddAuthorization();
+    }
+    
+    public static void RegisterMiddleware(this WebApplication app) {
+        app.UseExceptionHandler();
+        app.UseStatusCodePages();
+        
+        if (app.Environment.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseSerilogRequestLogging();
 
-        services.AddAuthorization();
-        return services;
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        RouteGroupBuilder api = app.MapGroup("api");
+        api.RegisterUserEndpoints();
     }
 }
